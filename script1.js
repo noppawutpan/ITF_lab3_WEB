415102516b4aa22de4d6f4c03e4eb07bac0492cf
noppawutpan <68070247@kmitl.ac.th>
2025-10-03 14:48:25 +0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+const myinput = document.getElementById("myinput")
+
+function add() {
+    myinput.value = Number(myinput.value) + 1
+}
+
+function minus() {
+    myinput.value = Number(myinput.value) - 1
+}
